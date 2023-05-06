d735a21e2576c4531994dfbce5b64ca183c9ece6
Felipe Gabriel <felipe.faria@znc.com.br>
2023-05-06 10:16:15 -0300

    Adaptado para deploy no vercel

---
+++ index.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from raspagem import busca_pessoa
+
+
+app = Flask(__name__)
+
+
+app.config['JSON_AS_ASCII'] = False
+
+@app.route("/")
+def inicio():
+    dados = busca_pessoa()
+    return jsonify(dados)
+
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
